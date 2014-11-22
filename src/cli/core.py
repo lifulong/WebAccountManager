bec4139b7d1c1fc8ad9f0090eaa65c835d224b22
moslifulong <msl.fulong@gmail.com>
2014-11-23 01:34:25 +0800

    Modify account-manager client

--- src/cli/core.py
+++ src/cli/core.py
@@ -9,30 +9,76 @@ class web_account_manager:
 		Transfer data with account-manager tcp server.
 	"""
 
-	def __init__(self):
+	def __init__(self, svr_address, keep_alive=False, iomodule="async"):
+
+		self._socket = None
+		self.is_alive = False
+
+		if keep_alive:
+			try:
+				self._socket = socket.socket(AF_INET, SOCK_STREAM, 0)
+				self._socket.connect(svr_address)
+				self.rfile = self._socket.makefile("rb")
+				self.wfile = self._socket.makefile("wb")
+				self.is_alive = True
+			except:
+				self._socket = None
+				self.is_alive = False
+
+	def send_header(self):
+		""" Send header to peer server.
+
+			Write header info to socket wfile.
+		"""
 		pass
+
+	def send_head(self):
+		pass
+
+
+	def get_value(self):
+		""" Parse json response.
+
+			Return dict.
+		"""
+		pass
+
+	def make_request(self):
+		pass
+
+	def handle_command(self, cmd, **kwargs):
+		""" handle one usr command.
+
+			Support Async IO Module, cmd & command_dict required by this method.
+		"""
+
+		json_str = json.dumps(kwargs, sort_keys=True, separater=(',', ':'))
 	
-	def register(self):
+	def register(self, usr, email, passwd):
+
 		self.usr = usr
-		self.passwd = 
+		command_dict["usr"] = usr
+		command_dict["email"] = email
+		command_dict["passwd"] = passwd
+		self.handle_command("REGISTER", command_dict)
+		command_dict.clear()
+
+	def login(self, usr, passwd):
 		pass
 
-	def login(self):
+	def logout(self, usr):
 		pass
 
-	def logout(self):
+	def get_records(self, usr):
 		pass
 
-	def get_records(self):
+	def put_records(self, usr):
 		pass
 
-	def put_records(self):
+	def del_records(self, usr):
 		pass
 
-	def del_records(self):
-		pass
-
-	def mod_record(self):
+	def mod_record(self, usr):
 		pass
 
 
